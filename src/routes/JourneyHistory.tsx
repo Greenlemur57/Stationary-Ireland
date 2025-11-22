183111c441fb01fc0169d254125930ccff0d21a3
xWires <xwires@tangledwires.xyz>
2025-11-22 13:46:29 +0000

    Wrap journey history in <List>

--- src/routes/JourneyHistory.tsx
+++ src/routes/JourneyHistory.tsx
@@ -1,5 +1,5 @@
 import PageHeader from "../components/PageHeader.tsx";
-import {Card, CardBody, CardFooter, CardTitle, Icon, PageSection} from "@patternfly/react-core";
+import {Card, CardBody, CardFooter, CardTitle, Icon, List, ListItem, PageSection} from "@patternfly/react-core";
 import AngleRightIcon from '@patternfly/react-icons/dist/esm/icons/angle-right-icon';
 import {storage} from "../utils/storage.ts";
 import {Stations} from "../utils/station.ts";
@@ -9,19 +9,23 @@ export default async function JourneyHistory() {
     <>
       <PageHeader title="Journey history" />
       <PageSection>
-        {(await storage.getJourneys()).map((j) => (
-          <Card key={j.uuid}>
-            <CardTitle>
-              {Stations[j.stations[0]].displayName} <Icon><AngleRightIcon /></Icon> {Stations[j.stations[j.stations.length - 1]].displayName}
-            </CardTitle>
-            <CardBody>
-              Stations: {j.stations.map((s) => Stations[s].displayName).join(", ")}
-            </CardBody>
-            <CardFooter>
-              {new Date(j.timestamp).toLocaleDateString("en-GB")}
-            </CardFooter>
-          </Card>
-        ))}
+        <List isPlain>
+          {(await storage.getJourneys()).map((j) => (
+            <ListItem>
+              <Card key={j.uuid}>
+                <CardTitle>
+                  {Stations[j.stations[0]].displayName} <Icon><AngleRightIcon /></Icon> {Stations[j.stations[j.stations.length - 1]].displayName}
+                </CardTitle>
+                <CardBody>
+                  Stations: {j.stations.map((s) => Stations[s].displayName).join(", ")}
+                </CardBody>
+                <CardFooter>
+                  {new Date(j.timestamp).toLocaleDateString("en-GB")}
+                </CardFooter>
+              </Card>
+            </ListItem>
+          ))}
+        </List>
       </PageSection>
     </>
   );
